65b92e2e7850def261a9d566b09633dc14baef61
Elwin Stephan <elwin.stephan@gmail.com>
2017-09-16 16:10:46 +0200

    Some changes to json

--- hello.py
+++ hello.py
@@ -9,7 +9,11 @@ def index():
     for curTile in range(0, 16):
         slices.append(analyse(curTile))
 
-    average = averageSlices(slices)
+	sliceValues = []
+	for slice in slices:
+		sliceValues.append(slice['values'])
+
+    average = averageSlices(sliceValues)
 
     result = {
         'contaminated': contaminated(average),
@@ -28,29 +32,53 @@ def index():
 def analyse(tile):
     slice = {
         'coordinate' : {
-            'x': tile,
-            'y': tile,
+            'x': tile % 4,
+            'y': tile / 4,
         },
-        'clean': 0.5,
-        'quinoa': 0.2,
-        'straw': 0.1,
-        'wheat': 0.2
+        'values': getRandomValues()
     }
 
     return slice
 
-def averageSlices(slices):
+def averageSlices(sliceValues):
     result = reduce(lambda x, y: {
         'clean': x['clean'] + y['clean'],
-        'quinoa': x['quinoa'] + y['quinoa'],
-        'wheat': x['wheat'] + y['wheat'],
         'straw': x['straw'] + y['straw'],
-        }, slices)
+        'pumpkin': x['pumpkin'] + y['pumpkin'],
+        'red_beans': x['red_beans'] + y['red_beans'],
+        'stones': x['stones'] + y['stones'],
+        'quinoa': x['quinoa'] + y['quinoa'],
+        'lentils': x['lentils'] + y['lentils'],
+        'dried_bean': x['dried_bean'] + y['dried_bean'],
+        'fines': x['fines'] + y['fines'],
+        }, sliceValues)
 
     for key, value in result.items():
-        result[key] = value / len(slices)
+        result[key] = value / len(sliceValues)
 
     return result
 
 def contaminated(overall):
-    return overall['clean'] >= 0.5
+    return overall['clean'] >= 0.1
+
+def getRandomValues():
+	values = {
+		'clean': random.random(),
+		'straw': random.random(),
+		'pumpkin': random.random(),
+		'red_beans': random.random(),
+		'stones': random.random(),
+		'quinoa': random.random(),
+		'lentils': random.random(),
+		'dried_bean': random.random(),
+		'fines': random.random()
+	}
+
+	sum = 0
+	for key, value in values.items():
+		sum += value
+
+	for key, value in values.items():
+		values[key] = value / sum
+
+	return values
