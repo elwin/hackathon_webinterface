599990739e89b47084f760f1b13ef6174668d166
Alexandre Moeri <alex.moeri@gmail.com>
2017-09-16 22:04:16 +0200

    mouse psotioning

--- public/buehler.js
+++ public/buehler.js
@@ -1,14 +1,22 @@
 angular.module('buehlerApp', []).controller ('buehlerController', ['$scope', '$http', function ($scope, $http) {    
     $scope.getResult = function() {
         $scope.gettingResult = true;
-        $http.get('http://localhost:5000/result').then(function(response){
+        $http.get('/result').then(function(response){
             $scope.result = response.data;
             console.log($scope.result);
             $scope.gettingResult = false;
         });
         $('#sample').mousemove(function(e){
-            console.log(e.pageX - this.offsetLeft);
-            console.log(e.pageY - this.offsetTop);
+            //console.log(e.pageX - this.offsetLeft);
+            //console.log(e.pageY - this.offsetTop);
+            mouseX = e.pageX - this.offsetLeft;
+            mouseY = e.pageY - this.offsetTop;
+            sizing = $scope.result.slicing.size;
+            $.each($scope.result.slices, function(i, slice){
+                if(mouseX >= slice.x && mouseX < (slice.x + sizing) && mouseY >= slice.y && mouseY < slice.y + sizing) {
+                    console.log(slice.category);
+                }
+            })
         }); 
     };
     $scope.getResult();
