e81bb3e5b0138cb7566edfd89d46853ccf471444
Elwin Stephan <elwin.stephan@gmail.com>
2017-09-16 23:34:52 +0200

    asdfasdf

--- public/buehler.js
+++ public/buehler.js
@@ -1,4 +1,4 @@
-angular.module('buehlerApp', []).controller ('buehlerController', ['$scope', '$http', function ($scope, $http) {    
+angular.module('buehlerApp', []).controller ('buehlerController', ['$scope', '$http', function ($scope, $http) {
      $scope.gettingResult = true;
     $scope.currentContamination = "clean";
     $scope.getResult = function() {
@@ -7,21 +7,24 @@ angular.module('buehlerApp', []).controller ('buehlerController', ['$scope', '$h
             $scope.result = response.data;
             console.log($scope.result);
             $scope.gettingResult = false;
+            $scope.$apply();
         });
-        $('#sample').mousemove(function(e){
-            //console.log(e.pageX - this.offsetLeft);
-            //console.log(e.pageY - this.offsetTop);
-            mouseX = e.pageX - this.offsetLeft;
-            mouseY = e.pageY - this.offsetTop;
-            sizing = $scope.result.slicing.size;
-            $.each($scope.result.slices, function(i, slice){
-                if(mouseX >= slice.x && mouseX < (slice.x + sizing) && mouseY >= slice.y && mouseY < slice.y + sizing) {
-                    $scope.currentContamination = slice.category;
-                    $scope.$apply();
-                }
-            })
-        }); 
     };
+
+    $('#sample').mousemove(function(e){
+        //console.log(e.pageX - this.offsetLeft);
+        //console.log(e.pageY - this.offsetTop);
+        mouseX = e.pageX - this.offsetLeft;
+        mouseY = e.pageY - this.offsetTop;
+        sizing = $scope.result.slicing.size;
+        $.each($scope.result.slices, function(i, slice){
+            if(mouseX >= slice.x && mouseX < (slice.x + sizing) && mouseY >= slice.y && mouseY < slice.y + sizing) {
+                $scope.currentContamination = slice.category;
+                $scope.$apply();
+            }
+        })
+    });
+
     $scope.getResult();
 }]).filter('percentage', ['$filter', function ($filter) {
   return function (input, decimals) {
