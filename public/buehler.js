86a0b9c98ef777139bb19aa33c7c40faa56f6b0d
Alexandre Moeri <alex.moeri@gmail.com>
2017-09-16 19:07:15 +0200

    add content to FE

--- public/buehler.js
+++ public/buehler.js
@@ -1,5 +1,20 @@
-angular.module('buehlerApp', []).controller ('buehlerController', ['$scope', '$http', function ($scope, $http) {
-  $http.get('http://localhost:5000/result').then(function(response){
-        $scope.result = response.data;
+angular.module('buehlerApp', []).controller ('buehlerController', ['$scope', '$http', function ($scope, $http) {    
+    $scope.getResult = function() {
+        $scope.gettingResult = true;
+        $http.get('http://localhost:5000/result').then(function(response){
+            $scope.result = response.data;
+            console.log($scope.result);
+            $scope.gettingResult = false;
+        });
+        $('#sample').mousemove(function(e){
+            console.log(e.pageX - this.offsetLeft);
+            console.log(e.pageY - this.offsetTop);
+        }); 
     });
+    };
+    $scope.getResult();
+}]).filter('percentage', ['$filter', function ($filter) {
+  return function (input, decimals) {
+    return $filter('number')(input * 100, decimals) + '%';
+  };
 }]);
