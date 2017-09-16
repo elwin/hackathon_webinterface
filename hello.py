48eb7acc2d7db36d24c8cf8e4a779fc50ba7bec1
Elwin Stephan <elwin.stephan@gmail.com>
2017-09-16 16:23:44 +0200

    Added static stuff

--- hello.py
+++ hello.py
@@ -1,66 +1,77 @@
 import random, datetime
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, send_from_directory
 from functools import reduce
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+	return jsonify(getResult())
 
-    slices = []
-    for curTile in range(0, 16):
-        slices.append(analyse(curTile))
+@app.route('/dynamic')
+def other():
+	return render_template('hello.html')
+
+@app.route('/static')
+def alsdkjf():
+	return send_from_directory('templates', 'hello.html')
+
+# Internal Methods #
+
+def getResult():
+	slices = []
+	for curTile in range(0, 16):
+		slices.append(analyse(curTile))
 
 	sliceValues = []
 	for slice in slices:
 		sliceValues.append(slice['values'])
 
-    average = averageSlices(sliceValues)
+	average = averageSlices(sliceValues)
 
-    result = {
-        'contaminated': contaminated(average),
-        'overall': average,
-        'slices': slices,
-        'slicing': {
-            'x_max': 4,
-            'y_max': 4
-        },
-        'time': datetime.datetime.now()
-    }
+	result = {
+		'contaminated': contaminated(average),
+		'overall': average,
+		'slices': slices,
+		'slicing': {
+			'x_max': 4,
+			'y_max': 4
+		},
+		'time': datetime.datetime.now()
+	}
 
-    return jsonify(result)
-
+	return result
 
 def analyse(tile):
-    slice = {
-        'coordinate' : {
-            'x': tile % 4,
-            'y': tile / 4,
-        },
-        'values': getRandomValues()
-    }
+	slice = {
+		'coordinate' : {
+			'x': tile % 4,
+			'y': tile / 4,
+		},
+		'values': getRandomValues()
+	}
 
-    return slice
+	return slice
 
 def averageSlices(sliceValues):
-    result = reduce(lambda x, y: {
-        'clean': x['clean'] + y['clean'],
-        'straw': x['straw'] + y['straw'],
-        'pumpkin': x['pumpkin'] + y['pumpkin'],
-        'red_beans': x['red_beans'] + y['red_beans'],
-        'stones': x['stones'] + y['stones'],
-        'quinoa': x['quinoa'] + y['quinoa'],
-        'lentils': x['lentils'] + y['lentils'],
-        'dried_bean': x['dried_bean'] + y['dried_bean'],
-        'fines': x['fines'] + y['fines'],
-        }, sliceValues)
+	result = reduce(lambda x, y: {
+		'clean': x['clean'] + y['clean'],
+		'straw': x['straw'] + y['straw'],
+		'pumpkin': x['pumpkin'] + y['pumpkin'],
+		'red_beans': x['red_beans'] + y['red_beans'],
+		'stones': x['stones'] + y['stones'],
+		'quinoa': x['quinoa'] + y['quinoa'],
+		'lentils': x['lentils'] + y['lentils'],
+		'dried_bean': x['dried_bean'] + y['dried_bean'],
+		'fines': x['fines'] + y['fines'],
+		}, sliceValues)
 
-    for key, value in result.items():
-        result[key] = value / len(sliceValues)
+	for key, value in result.items():
+		result[key] = value / len(sliceValues)
 
-    return result
+	return result
 
 def contaminated(overall):
-    return overall['clean'] >= 0.1
+	return overall['clean'] >= 0.1
 
 def getRandomValues():
 	values = {
